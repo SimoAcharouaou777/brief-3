bd180533cd13c57b7788c6ce0bc644fb827f53b3
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-10-26 10:52:32 +0100

    burger-menu,validation

--- innovation.js
+++ innovation.js
@@ -76,19 +76,20 @@ function passwordcheck(){
    }
 }
 
+
 // --------------------------DROP DOWN---------------------
 
 const Question = document.querySelectorAll('.Questions');
 const Answer = document.querySelectorAll('.Answer');
+
 for (let i = 0; i < Question.length; i++) {
     Question[i].addEventListener('click', function () {
-        if (Answer[i].style.display === "none") {
-            Answer[i].style.display == "block"
+        if (Answer[i].style.display === '' || Answer[i].style.display === 'none') {
+            Answer[i].style.display = 'block';
         } else {
-            Answer[i].style.display == "none"
+            Answer[i].style.display = 'none';
         }
-
-    })
+    });
 }
 
 
